adff3d828841b2117ce52b5fa92cf905c4b7e33c
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-01 23:05:05 +0530

    Sample.jsx - Created Grape constant - yet to test

--- react-demo-project/src/example/Sample.jsx
+++ react-demo-project/src/example/Sample.jsx
@@ -4,6 +4,10 @@ const Mango=()=>{
     return(<h2>Mangoes are sweet</h2>)
     }
 
+const Grapes=()=>{
+    return(<h2>Grapes are good</h2>)
+    }
+
 function Sample(){
     return(
         <div>Sample</div>)
