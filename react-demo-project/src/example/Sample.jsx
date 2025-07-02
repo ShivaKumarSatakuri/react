889bfda35314b1ed3924d7d8d2454f3d0894dc82
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-02 22:27:36 +0530

    - Added arrow funcctions - Formatted code

--- react-demo-project/src/example/Sample.jsx
+++ react-demo-project/src/example/Sample.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
-const Mango=()=>{
-    return(<h2>Mangoes are sweet</h2>)
-    }
+// arrow function
+const Mango = () => {
+    return (<h2>Mangoes are sweet</h2>)
+}
 
-const Grapes=()=>{
-    return(<h2>Grapes are good</h2>)
-    }
+const Grapes = () => {
+    return (<h2>Grapes are good</h2>)
+}
 
-function Sample(){
-    return(
-        <div>Sample</div>)
-    }
+function Sample() {
+    return (
+        <div>Sample
+            <Mango />
+            <Grapes />
+        </div>
+    )
+}
 
-export default Sample
+export default Sample;
