8b0ab9c0518d02a1963fdc288cb2f45be7de422f
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-03 23:33:11 +0530

    Created CSS components and tested them

--- react-demo-project/src/App.js
+++ react-demo-project/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Apple from './Apple'
 import Sample from './example/Sample';
 
+import './sample.css'
+
 function App() {
     return (
         <div className="App">
             <Apple />
             <Sample />
+            <h1>This is Sample CSS File</h1>
         </div>
     );
 }
