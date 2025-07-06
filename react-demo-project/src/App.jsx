3c7d0ed69fdb7d68d82e1f381c93e27e79f864fa
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-06 20:43:48 +0530

    App.jsx - Added variables and passed values dynamically

--- react-demo-project/src/App.jsx
+++ react-demo-project/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 function App() {
+
+    let a = "Apples"
+    let b = "Bananas"
+
     return (
-        <div>
-            <h1>Javascript XML</h1>
-        </div>
+        //<div>
+        //    <h1>Javascript XML</h1>
+        //</div>
+
+        <section>
+            <h1>I like {a} & {b}</h1>
+        </section>
     )
 }
 
