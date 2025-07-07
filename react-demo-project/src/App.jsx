92ef8a08494265e41e1d1d8ed08ca25de07225b8
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-07 22:02:51 +0530

    App.jsx - Added image

--- react-demo-project/src/App.jsx
+++ react-demo-project/src/App.jsx
@@ -17,6 +17,8 @@ function App() {
 
         <div className="firstElement">
             <p className="elementPara">Created first element</p>
+
+            <img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg"/>
         </div>
     )
 }
