645c1b2a075b612882c4396d073b3cf9e65fee25
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-10 18:38:57 +0530

    Added useEfffect implementation

--- react-demo-project/src/StateMgmt.jsx
+++ react-demo-project/src/StateMgmt.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const StateMgmt = () => {
     const [city, setCity] = useState("Hyderabad")
 
+    //useEffect
+    useEffect(() => {
+        if (city === "Hyderabad") {
+            setCity("Bangalore")
+        } else {
+            setCity("Goa")
+        }
+    }, [city])
     console.log(city)
 
     //testing setState function
     /*setCity("Mumbai")
     console.log(city)*/
 
+    /* this logic is specific to useState
     if (city === "Hyderabad") {
         setCity("Bangalore")
     } else {
         setCity("Goa")
-    }
+    }*/
 
     return (
         <div>
