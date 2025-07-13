2f01e08e14bc23455c3705c973e8e71c46ba5df8
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-07-13 21:33:57 +0530

    Implemented reset

--- react-demo-project/src/ClickEvent.jsx
+++ react-demo-project/src/ClickEvent.jsx
@@ -13,11 +13,16 @@ const ClickEvent = () => {
         setNumber(number <= 0 ? 0 : number - 1)
     }
 
+    const reset = () => {
+        setNumber(0)
+    }
+
     return (
         <div>
             <h1>{number}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
